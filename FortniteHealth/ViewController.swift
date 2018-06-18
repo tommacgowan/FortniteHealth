e75a346296a052821bbf4092f06daae827bf1782
Tom Macgowan <tommacgowan@toms-mbp.gateway>
2018-06-18 18:08:43 +0930

    Initial Commit

---
+++ FortniteHealth/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  FortniteHealth
+//
+//  Created by Tom Macgowan on 18/6/18.
+//  Copyright © 2018 Tom Macgowan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
